731c5f7cfb9cbb588de8776746968ab2b9867bd6
youness <younesserbai@gmail.com>
2023-10-26 01:44:01 +0200

    JavaScript Increment and  Decrement

--- detail.js
+++ detail.js
@@ -4,3 +4,19 @@ function show() {
 function aficher() {
   document.getElementsByClassName("side_bar")[0].classList.toggle("return");
 }
+let plus = document.getElementById("addToCart");
+let moin = document.getElementById("deletToCart");
+let conteur = document.getElementById("count");
+
+plus.addEventListener("click", function () {
+  conteur.textContent++;
+  Total.textContent = Total.textContent - -Price.textContent;
+});
+moin.addEventListener("click", function () {
+  if (conteur.textContent > 0 && Total.textContent > 0) {
+    conteur.textContent--;
+    Total.textContent -= Price.textContent;
+  }
+});
+let Price = document.getElementById("price");
+let Total = document.getElementById("total");
--- product_list.js
+++ product_list.js
@@ -4,3 +4,19 @@ function show() {
 function delet() {
   document.getElementsByClassName("shopping_cart")[0].classList.toggle("maske");
 }
+let plus = document.getElementById("addToCart");
+let moin = document.getElementById("deletToCart");
+let conteur = document.getElementById("count");
+
+plus.addEventListener("click", function () {
+  conteur.textContent++;
+  Total.textContent = Total.textContent - -Price.textContent;
+});
+moin.addEventListener("click", function () {
+  if (conteur.textContent > 0 && Total.textContent > 0) {
+    conteur.textContent--;
+    Total.textContent -= Price.textContent;
+  }
+});
+let Price = document.getElementById("price");
+let Total = document.getElementById("total");
